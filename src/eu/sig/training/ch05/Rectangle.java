0029afd6945ac5b2f7c22128faa99615c890b79d
Sebastian Brzozowski <sbrzozowski@7bulls.com>
2016-11-22 15:34:09 +0100

    BoardPanel refactor

---
+++ src/eu/sig/training/ch05/Rectangle.java
@@ -0,0 +1,41 @@
+package eu.sig.training.ch05;
+
+public class Rectangle {
+
+    private int x;
+    private int y;
+    private int w;
+    private int h;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(final int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(final int y) {
+        this.y = y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(final int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(final int h) {
+        this.h = h;
+    }
+}
